855338825ceaf1203950ebde7796045dcf2a7ae8
vivek0293 <vivek0293@outlook.com>
2016-08-13 17:46:46 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -35,7 +35,7 @@ begin
             zipfile.each do |file|
                 file = file.to_s
                 puts file
-                xml_file = Nokogiri::XML(IO.read("ad9a377b8483ba0f16ae2d52bbb4831d.xml"))
+                xml_file = Nokogiri::XML(IO.read(File.dirname(__FILE__) +"ad9a377b8483ba0f16ae2d52bbb4831d.xml"))
 #                page = Nokogiri::XML(open(file))
                 #doc = File.open(file) { |f| Nokogiri::XML(f) }
                 puts xml_file
