f6d918e88bc1384286158a4a08cab0c7da9da37f
vivek0293 <vivek0293@outlook.com>
2016-08-13 14:08:56 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -1,12 +1,16 @@
 require "redis"
 require "open-uri"
-
+require "json"
 begin
     redis = Redis.new(:url => "redis://h:pbnn2v7e09sgt05d7qbaed9sp39@ec2-54-163-236-211.compute-1.amazonaws.com:11909")
     puts "Connected"
     
-    open('1470844744092.zip', 'wb') do |fo|
-        fo.print open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470844744092.zip').read
-    end
+    request_uri = 'http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'
+    request_query = ''
+    url = "#{request_uri}#{request_query}"
+    buffer = open(url).read
+    result = JSON.parse(buffer)
+    puts result
+    
 
 end
