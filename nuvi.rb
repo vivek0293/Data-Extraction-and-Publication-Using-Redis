b3f50588b43a1f97003cf9dfd979a07667018d28
vivek0293 <vivek0293@outlook.com>
2016-08-13 18:03:02 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -28,21 +28,26 @@ begin
 ##links = page.css("a")[5]
 ##link = Nokogiri::HTML(open(links))
 #puts page
+#
+#    open('1470866577151.zip', 'wb') do |file|
+#        file.print open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470866577151.zip').read
+#        Zip::File.open("1470866577151.zip") do |zipfile|
+#            zipfile.each do |file|
+#                file = file.to_s
+#                puts file
+#                puts"enter:"
+#                a = gets.chomp()
+#                xml_file = Nokogiri::XML(IO.read(a))
+##                page = Nokogiri::XML(open(file))
+#                #doc = File.open(file) { |f| Nokogiri::XML(f) }
+#                puts xml_file
+#                # do something with file
+#            end
+#        end
+#    end
 
-    open('1470866577151.zip', 'wb') do |file|
-        file.print open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470866577151.zip').read
-        Zip::File.open("1470866577151.zip") do |zipfile|
-            zipfile.each do |file|
-                file = file.to_s
-                puts file
-                puts"enter:"
-                a = gets.chomp()
-                xml_file = Nokogiri::XML(IO.read(a))
-#                page = Nokogiri::XML(open(file))
-                #doc = File.open(file) { |f| Nokogiri::XML(f) }
-                puts xml_file
-                # do something with file
-            end
-        end
+    open('1470870158149.zip', 'wb') do |file|
+        down = file.print open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470870158149.zip')
+        down.read
     end
 end
