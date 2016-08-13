606847e170953fba4c4babb879619310b9d2e29c
vivek0293 <vivek0293@outlook.com>
2016-08-13 16:02:12 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -23,9 +23,11 @@ begin
 #    link = Nokogiri::HTML(open(links))
 #    puts link
 
-page = Nokogiri::HTML(open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470858984479.zip"))
-#links = page.css("a")[5]
-#link = Nokogiri::HTML(open(links))
-puts page
+#page = Nokogiri::HTML(open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470858984479.zip"))
+##links = page.css("a")[5]
+##link = Nokogiri::HTML(open(links))
+#puts page
 
+open('1470862638440.zip', 'wb') do |file|
+    file << open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1470862638440.zip').read
 end
