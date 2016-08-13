6b3fc1251458d2817cbab2233bbe925061fe6c94
vivek0293 <vivek0293@outlook.com>
2016-08-13 16:42:47 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -2,7 +2,7 @@ require "redis"
 require "open-uri"
 require "json"
 require "nokogiri"
-require "rubyzip"
+require "zip"
 begin
     redis = Redis.new(:url => "redis://h:pbnn2v7e09sgt05d7qbaed9sp39@ec2-54-163-236-211.compute-1.amazonaws.com:11909")
     puts "Connected"
