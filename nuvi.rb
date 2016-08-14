5425ad8187cef50077518bdfed6022fe44d08fc9
vivek0293 <vivek0293@outlook.com>
2016-08-14 01:04:36 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -20,45 +20,20 @@ begin
     redis.flushall
 
     i = 5
-
-    puts "Enter Choice as '1' for running testCase; '2' for running the entire data:"
-    choice = gets.chomp.to_i
-
-    if choice == '1'
-        while i<=8 do
-            open(a[i], 'wb') do |file|
-                file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
-                Zip::File.open(a[i]) do |zipfile|
-                    zipfile.each do |file|
-                        xml = zipfile.read(file)
-                        doc = Nokogiri::XML(xml)
-                        title = doc.at_xpath('//discussion_title').text
-                        text = doc.at_xpath('//topic_text').text
-                        puts title
-                        redis.set(title,text)
-                    end
+    
+    while i<=8 do
+        open(a[i], 'wb') do |file|
+            file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
+            Zip::File.open(a[i]) do |zipfile|
+                zipfile.each do |file|
+                    xml = zipfile.read(file)
+                    doc = Nokogiri::XML(xml)
+                    title = doc.at_xpath('//discussion_title').text
+                    text = doc.at_xpath('//topic_text').text
+                    redis.set(title,text)
                 end
             end
-            i=i+1
         end
-    if choice == '2'
-        while i<=length do
-            open(a[i], 'wb') do |file|
-                file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
-                Zip::File.open(a[i]) do |zipfile|
-                    zipfile.each do |file|
-                        xml = zipfile.read(file)
-                        doc = Nokogiri::XML(xml)
-                        title = doc.at_xpath('//discussion_title').text
-                        text = doc.at_xpath('//topic_text').text
-                        redis.set(title,text)
-                    end
-                end
-            end
-            i=i+1
-        end
-    else
-        puts"invalid choice"
+        i=i+1
     end
-  end
 end
