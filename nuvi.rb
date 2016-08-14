2ba8e57fbd365ca6009af80325f395853bc17fc6
vivek0293 <vivek0293@outlook.com>
2016-08-14 13:47:03 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -20,6 +20,7 @@ begin
     redis.flushall
 
     i = 5
+    out = []
     puts "Enter Choice as '1' for testCase; '2' for entire data:"
     choice = gets.chomp.to_i
     if choice == 1
@@ -33,39 +34,52 @@ begin
                         doc = Nokogiri::XML(xml)
                         title = doc.at_xpath('//discussion_title').text
                         text = doc.at_xpath('//topic_text').text
-                        redis.set(title,text)
+                        if out.include?text
+                            break
+                        else
+                            out.push(text)
+                            redis.rpush(NEWS_XML,text)
+                        #redis.set(title,text)
                     end
                 end
             end
             i=i+1
         end
         puts"Insertion Completed"
-        key = redis.keys('*')
-        puts key
-        ans = gets.chomp.to_s
-        out = redis.get(ans)
-        puts out
-        
-    elsif choice == 2
-    puts"Populating data...."
-    while i<=length do
-        open(a[i], 'wb') do |file|
-            file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
-            Zip::File.open(a[i]) do |zipfile|
-                zipfile.each do |file|
-                    xml = zipfile.read(file)
-                    doc = Nokogiri::XML(xml)
-                    title = doc.at_xpath('//discussion_title').text
-                    text = doc.at_xpath('//topic_text').text
-                    redis.set(title,text)
-                end
-            end
-        end
-        i=i+1
-    end
-    puts"Insertion Completed"
+#        key = redis.keys('*')
+#        puts key
+#        puts"Enter a title:"
+#        ans = gets.chomp.to_s
+#        out = redis.get(ans)
+#        puts out
 
-    else
-        puts"Invalid Choice"
+#    elsif choice == 2
+#        puts"Populating data...."
+#        while i<=length do
+#            open(a[i], 'wb') do |file|
+#                file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
+#                Zip::File.open(a[i]) do |zipfile|
+#                    zipfile.each do |file|
+#                        xml = zipfile.read(file)
+#                        doc = Nokogiri::XML(xml)
+#                        title = doc.at_xpath('//discussion_title').text
+#                        text = doc.at_xpath('//topic_text').text
+#                        redis.set(title,text)
+#                    end
+#                end
+#            end
+#            i=i+1
+#        end
+#        puts"Insertion Completed"
+#        key = redis.keys('*')
+#        puts key
+#        puts"Enter a title:"
+#        ans = gets.chomp.to_s
+#        out = redis.get(ans)
+#        puts out
+#
+#
+#    else
+#        puts"Invalid Choice"
     end
 end
