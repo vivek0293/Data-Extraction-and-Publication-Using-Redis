f89d87838fdbbb1dd4c374e85eb9e0131845d80b
vivek0293 <vivek0293@outlook.com>
2016-08-14 01:44:26 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -33,7 +33,7 @@ begin
                         doc = Nokogiri::XML(xml)
                         title = doc.at_xpath('//discussion_title').text
                         text = doc.at_xpath('//topic_text').text
-                        redis.set(title,text)
+                        redis.hset(NEWS_XML,title,text)
                     end
                 end
             end
