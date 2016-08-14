a99d069a036c9695da891c7efba0e6be3ac2211b
vivek0293 <vivek0293@outlook.com>
2016-08-14 00:24:56 -0500

    Demo

--- nuvi.rb
+++ nuvi.rb
@@ -35,28 +35,28 @@ begin
 ##links = page.css("a")[5]
 ##link = Nokogiri::HTML(open(links))
 #puts page
-#redis.flushall
-redis.get("Germany agonises over role of army at home after attacks")
-#    i = 5
-#    while i<=10 do
-#        open(a[i], 'wb') do |file|
-#            file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
-#            Zip::File.open(a[i]) do |zipfile|
-#                zipfile.each do |file|
-#                    xml = zipfile.read(file)
-#                    doc = Nokogiri::XML(xml)
-#                    title = doc.at_xpath('//discussion_title')
-#                    text = doc.at_xpath('//topic_text')
-#                    #puts title
-#                    #redis.set(title,text)
-#                    redis.get("Germany agonises over role of army at home after attacks")
-#                    #puts i
-#                    puts"entered"
-#                    #puts xml
-#
-#                end
-#            end
-#        end
-#        i=i+1
-#    end
+redis.flushall
+#redis.get("Germany agonises over role of army at home after attacks")
+    i = 5
+    while i<=10 do
+        open(a[i], 'wb') do |file|
+            file.write open('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'+a[i]).read
+            Zip::File.open(a[i]) do |zipfile|
+                zipfile.each do |file|
+                    xml = zipfile.read(file)
+                    doc = Nokogiri::XML(xml)
+                    title = doc.at_xpath('//discussion_title').text
+                    text = doc.at_xpath('//topic_text').text
+                    puts title
+                    redis.set(title,text)
+                    #redis.get("Germany agonises over role of army at home after attacks")
+                    puts i
+                    puts"entered"
+                    #puts xml
+
+                end
+            end
+        end
+        i=i+1
+    end
 end
